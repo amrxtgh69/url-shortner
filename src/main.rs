cd3feb22cfdaa50cb1068a2942f6b757d90cf905
amrxtgh <ghimireamrit619@gmail.com>
2025-11-20 23:15:26 +0545

    change main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+use tokio;
 use std::{net::{SocketAddr, TcpListener}, sync::Arc, time::Duration};
 use axum::{
     Router,
     routing::{get, post},
 };
-use tokio;
 
 #[derive(Clone)]
 struct AppState {
@@ -15,12 +15,13 @@ struct AppState {
 
 #[tokio::main]
 async fn main() {
+    let app_state = Arc::new(AppState { db, redis, base_url });
     let app = Router::new()
         .route("/shorten", post(create_short))
         .route("/:code", get(redirect))
         .with_state(app_state);    
     
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
     println!("Server listening on port 3000");
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
